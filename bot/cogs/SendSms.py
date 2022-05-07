9bebfb79f6f94d8e45de5e53cb0fbd9b54b9f785
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-07 20:34:39 +0530

    doc + embed

--- bot/cogs/SendSms.py
+++ bot/cogs/SendSms.py
@@ -1,6 +1,7 @@
 import discord
 import telnyx
 from discord.ext import commands
+from datetime import datetime
 
 from bot.database.manager import main
 from bot.database.manager import telnyx_data
@@ -13,11 +14,20 @@ class SendSMS(commands.Cog, description="Send messages"):
 
         telnyx.api_key = telnyx_data.api_key
 
-    
     @commands.command()
     async def sms(self, ctx, number, *text):
+        """
+        Usage -->
+            >sms <to_number> *[text]
 
-        text  = " ".join(text)
+        Example -->
+            >sms +12345678910 any text that comes after the phone number to be sent will be the content of the message
+
+        Telynx API Documentation -->
+            https://developers.telnyx.com/docs/v2/messaging/quickstarts/sending-sms-and-mms?lang=python
+        """
+
+        text = " ".join(text)
 
         telnyx.Message.create(
             from_=telnyx_data.from_number,
@@ -25,8 +35,28 @@ class SendSMS(commands.Cog, description="Send messages"):
             text=text
         )
 
-        await ctx.send(f"Sent SMS:\nFrom: {telnyx_data.from_number}\nTo: {number}\nBy {ctx.author.name}\nContent: ``` {text} ```")
+        embed = discord.Embed(title="Bot Management",
+                              description="Change Phone Number",
+                              timestamp=datetime.utcnow(),
+                              color=0x00ffff)
+        embed.set_author(
+            name=str(self.client.user.name),
+            icon_url=str(self.client.user.avatar_url)
+        )
+        embed.add_field(
+            name="Old Phone Number",
+            value=str(telnyx_data.from_number),
+            inline=False
+        )
+        embed.add_field(
+            name="New Phone Number",
+            value=str(telnyx_data.from_number),
+            inline=False
+        )
+        embed.set_footer(text=f"Requested by {ctx.author.name}")
+        await ctx.send(embed=embed)
 
+        await ctx.send(f"Sent SMS:\nFrom: {telnyx_data.from_number}\nTo: {number}\nBy {ctx.author.name}\nContent: ``` {text} ```")
 
 
 def setup(client: commands.Bot):
