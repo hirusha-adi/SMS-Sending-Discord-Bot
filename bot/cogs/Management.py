3846523818f87d24bda9e6734fa5981de4dca4e1
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-07 20:28:00 +0530

    added embed

--- bot/cogs/Management.py
+++ bot/cogs/Management.py
@@ -1,6 +1,7 @@
 import discord
 import telnyx
 from discord.ext import commands
+from datetime import datetime
 
 from bot.database.manager import main
 from bot.database.manager import telnyx_data
@@ -15,11 +16,48 @@ class Management(commands.Cog, description="Manage Discord Bot. Only admins can 
     @commands.command()
     async def change_number(self, ctx, number):
         if int(ctx.author.id) in main.admins:
+            embed = discord.Embed(title="Bot Management",
+                                  description="Change Phone Number",
+                                  color=0x00ffff)
+            embed.set_author(
+                name=str(self.client.user.name),
+                icon_url=str(self.client.user.avatar_url)
+            )
+            embed.add_field(
+                name="Old Phone Number",
+                value=str(telnyx_data.from_number),
+                inline=False
+            )
+
+            telnyx_data.from_number = str(number)
             telnyx_manager.update(from_number=str(number))
-            await ctx.send(f"Updated")
-        
+
+            embed.add_field(
+                name="New Phone Number",
+                value=str(telnyx_data.from_number),
+                inline=False
+            )
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            await ctx.send(embed=embed)
+
         else:
-            await ctx.send(f"You do not have permission to change the number")
+            embed = discord.Embed(
+                title="An error has occured",
+                color=0xff0000,
+                timestamp=datetime.utcnow()
+            )
+            embed.set_author(
+                name=str(self.client.user.name),
+                icon_url=str(self.client.user.avatar_url)
+            )
+            embed.add_field(
+                name="Error:",
+                value="You do not have permission to use this command",
+                inline=False
+            )
+            embed.set_footer(text=f"Reuqested by {ctx.author.name}")
+            await ctx.send(embed=embed)
+
 
 def setup(client: commands.Bot):
     client.add_cog(Management(client))
