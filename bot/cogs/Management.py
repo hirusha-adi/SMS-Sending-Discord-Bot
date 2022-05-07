d06ad2c7fbb43fc333a6e88440fb2eb518436266
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-07 20:30:12 +0530

    change api key

--- bot/cogs/Management.py
+++ bot/cogs/Management.py
@@ -17,6 +17,7 @@ class Management(commands.Cog, description="Manage Discord Bot. Only admins can 
         if int(ctx.author.id) in main.admins:
             embed = discord.Embed(title="Bot Management",
                                   description="Change Phone Number",
+                                  timestamp=datetime.utcnow(),
                                   color=0x00ffff)
             embed.set_author(
                 name=str(self.client.user.name),
@@ -34,6 +35,52 @@ class Management(commands.Cog, description="Manage Discord Bot. Only admins can 
             embed.add_field(
                 name="New Phone Number",
                 value=str(telnyx_data.from_number),
+                inline=False
+            )
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            await ctx.send(embed=embed)
+
+        else:
+            embed = discord.Embed(
+                title="An error has occured",
+                color=0xff0000,
+                timestamp=datetime.utcnow()
+            )
+            embed.set_author(
+                name=str(self.client.user.name),
+                icon_url=str(self.client.user.avatar_url)
+            )
+            embed.add_field(
+                name="Error:",
+                value="You do not have permission to use this command",
+                inline=False
+            )
+            embed.set_footer(text=f"Reuqested by {ctx.author.name}")
+            await ctx.send(embed=embed)
+
+    @commands.command()
+    async def change_api_key(self, ctx, api_key):
+        if int(ctx.author.id) in main.admins:
+            embed = discord.Embed(title="Bot Management",
+                                  description="Change Telnyx API Key",
+                                  timestamp=datetime.utcnow(),
+                                  color=0x00ffff)
+            embed.set_author(
+                name=str(self.client.user.name),
+                icon_url=str(self.client.user.avatar_url)
+            )
+            embed.add_field(
+                name="Old Telynx API Key",
+                value=str(telnyx_data.api_key),
+                inline=False
+            )
+
+            telnyx_data.api_key = str(api_key)
+            telnyx_manager.update(api_key=str(api_key))
+
+            embed.add_field(
+                name="New Telynx API Key",
+                value=str(telnyx_data.api_key),
                 inline=False
             )
             embed.set_footer(text=f"Requested by {ctx.author.name}")
