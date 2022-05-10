d14aa850638eb6ac1f0716ec3b9004dd20b490b3
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-10 15:50:03 +0530

    help

--- bot/cogs/base.py
+++ bot/cogs/base.py
@@ -2,14 +2,16 @@ import platform
 
 import discord
 from discord.ext import commands
+from datetime import datetime
 
-from bot.database.manager.main import admins
+from database.manager.main import prefix, admins
 
 
 class Base(commands.Cog, description="Main stuff related to the bot"):
 
     def __init__(self, client: commands.Bot):
         self.client = client
+        self.client.remove_command('help')
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -24,6 +26,29 @@ class Base(commands.Cog, description="Main stuff related to the bot"):
             )
         )
         print(f'[*] Bot is ready to be used!')
+
+    @commands.command()
+    async def help(self, ctx):
+        final_help = f"{prefix}sms <to_number> *[text]\n"
+        if ctx.author.id in admins:
+            final_help += f"{prefix}change_number <number>\n"
+            final_help += f"{prefix}change_api_key <api_key>\n"
+
+        embed = discord.Embed(title=f"Help for {self.client.user.name}",
+                              description="All commands usage",
+                              timestamp=datetime.utcnow(),
+                              color=0x00ffff)
+        embed.set_author(
+            name=str(self.client.user.name),
+            icon_url=str(self.client.user.avatar_url)
+        )
+        embed.add_field(
+            name="Commands",
+            value=final_help,
+            inline=False
+        )
+        embed.set_footer(text=f"Requested by {ctx.author.name}")
+        await ctx.send(embed=embed)
 
 
 def setup(client: commands.Bot):
